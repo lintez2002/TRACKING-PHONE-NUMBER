cd96e292ad0ab555006172cf001c5a87e113c705
lintez2002 <101658970+lintez2002@users.noreply.github.com>
2022-03-16 15:51:35 +0530

    Add files via upload

---
+++ miniproject1.py
@@ -0,0 +1,18 @@
+import phonenumbers
+
+from test import number
+
+
+from phonenumbers import geocoder
+
+c_num = phonenumbers.parse(number,"CH")
+
+print(geocoder.description_for_number(c_num, "en"))
+
+
+
+from phonenumbers import carrier
+
+service_num = phonenumbers.parse(number, "RO")
+
+print(carrier.name_for_number(service_num, "en"))
